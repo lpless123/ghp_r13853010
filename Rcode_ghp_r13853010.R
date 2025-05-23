a344580fefe8384eae5bc2bd2003ab2901d67b04
lpless123 <r13853010@ntu.edu.tw>
2025-05-23 17:36:35 +0800

    Update Rcode_ghp_r13853010.R

--- Rcode_ghp_r13853010.R
+++ Rcode_ghp_r13853010.R
@@ -1,6 +1,6 @@
 library(rio)
 library(dplyr)
-setwd("/Users/lydiapless/Downloads/") #working directory can be modified to individual's own directory
+setwd("/Users/lydiapless/Downloads/") #working directory can be modified to individual's own directory path 
 getwd()
 dengue<- import("dengue_assignment.csv")
 (cases2014 <- dengue %>%
