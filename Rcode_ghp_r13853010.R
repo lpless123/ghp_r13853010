504ae8447e2d5a1941beba0d5df0c38702a9015d
lpless123 <r13853010@ntu.edu.tw>
2025-05-23 17:35:47 +0800

    Update Rcode_ghp_r13853010.R

--- Rcode_ghp_r13853010.R
+++ Rcode_ghp_r13853010.R
@@ -3,6 +3,6 @@ library(dplyr)
 setwd("/Users/lydiapless/Downloads/")
 getwd()
 dengue<- import("dengue_assignment.csv")
-(cases2014<- dengue %>%
+(cases2014 <- dengue %>%
   filter(year == 2014) %>%
   summarise(total_dengue_cases = sum(case_number, na.rm = TRUE)))
