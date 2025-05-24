1ff09e4b0a3ae9b9a6d94e95df069da6e11e7b06
lpless123 <r13853010@ntu.edu.tw>
2025-05-24 18:15:26 +0800

    Update Rcode_ghp_r13853010.R

--- Rcode_ghp_r13853010.R
+++ Rcode_ghp_r13853010.R
@@ -1,8 +1,9 @@
-library(rio) #rio and dplyr packages already installed on personal R studio
-library(dplyr) #rio and dplyr package must be installed first using the function install.packages("package_name") if individual has not already
+library(rio) #rio and dplyr package must be installed first using the function install.packages("package_name") if individual has not already
+library(dplyr) #rio and dplyr packages already installed on personal R studio
 setwd("/Users/lydiapless/Downloads/") #working directory can be modified to individual's own directory path 
 getwd()
 dengue<- import("dengue_assignment.csv")
 (cases2014 <- dengue %>%
   filter(year == 2014) %>%
   summarise(total_dengue_cases = sum(case_number, na.rm = TRUE)))
+
